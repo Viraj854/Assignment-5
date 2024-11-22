33646f0d864bd43f12cac03bc03b0e464bae2457
Viraj Parmar <viraj.parmar@student.tdsb.on.ca>
2024-11-22 13:09:29 -0500

    Added login view and register view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,26 +4,28 @@
 
 <template>
   <div class="hero-buttons">
-    <button type="button">Register</button>
-    <button type="button">Login</button>
+    <RouterLink to="/register" class="button register">Register</RouterLink>
+    <RouterLink to="/login" class="button login">Login</RouterLink>
   </div>
 </template>
 
 <style scoped>
-.hero-buttons button {
+.hero-buttons .button {
+  display: inline-block; 
   background-color: #4073ad;
   color: #ffffff;
   padding: 10px 30px;
   font-size: 18px;
+  text-align: center;
+  text-decoration: none; 
   border: none;
   margin: 5px;
   border-radius: 5px;
   cursor: pointer;
   transition: background-color 0.3s;
-
 }
 
-.hero-buttons button:hover {
+.hero-buttons .button:hover {
   background-color: #305a8a;
 }
 </style>
